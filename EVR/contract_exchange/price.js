0ec387940768ebac218c0df7e3d961cd1b12835a
qinxiaowen <453000697@qq.com>
2018-11-02 09:16:44 +0800

    remove  otc dex is not active

--- EVR/contract_exchange/price.js
+++
@@ -1,9 +0,0 @@
-function getCurrentBuyPrice(rows) {
-    // The rows are from the bancor table you specified
-    return 0.0000;
-}
-
-function getCurrentSellPrice(rows) {
-    // The rows are from the bancor table you specified
-    return 0.0000;
-}
