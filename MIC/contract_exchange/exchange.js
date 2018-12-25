940de60b8c002ff1a00e046d83474e3049bbd484
小TIAN.★☆ <1756768668@qq.com>
2018-12-25 17:35:44 +0800

    MIC token

---
+++ MIC/contract_exchange/exchange.js
@@ -0,0 +1,15 @@
+function buy() {
+    var contract_account = 'michaelzhu12';
+    var amount = $('#buyAmount').val();
+    // You can use the variable: account, requiredFields in this script
+    eos.contract('eosio.token', { requiredFields }).then(contract => {
+        return contract.transfer(
+            account.name, 
+            contract_account, 
+            parseFloat($('#buyAmount').val()).toFixed(4) + ' EOS', 
+            `buy`, 
+            { 
+                authorization: [`${account.name}@${account.authority}`] 
+            });
+    })
+}
